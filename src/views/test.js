96c02aafebe0e947ce85b06cef3e34cba1b8af93
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-05-29 12:38:33 +0530

    syntax highlighter

--- src/views/test.js
+++ src/views/test.js
@@ -1,34 +1,22 @@
-import React, {useEffect, useState, useRef, useMemo} from 'react';
-
-// if(window)
-//   import JoditEditor from "jodit-react";
+import React from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { PencilIcon } from "@heroicons/react/solid"
 
 const Example = () => {
-	const editor = useRef(null)
-	const [content, setContent] = useState('')
+  const codeString = `const hello = "world"
+const fun = () => {
+  return "hello";
+}`
 
-	const config = useMemo({
-		readonly: false ,
-		placeholder:'Start typings...'
-	}, [placeholder])
-
-  useEffect(() => {
-    
-  }, [])
-
-	return (
-    //         <JoditEditor
-    //         	ref={editor}
-    //             value={content}
-    //             config={config}
-		// tabIndex={1} // tabIndex of textarea
-		// onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-    //             onChange={newContent => {}}
-    //         />
-
-          <div></div>
-        )
+  return (
+    <div className="relative h-full">
+    <PencilIcon className='absolute h-8 w-8 top-2 right-2 z-50 bg-white rounded-full p-1 cursor-pointer' />
+    <SyntaxHighlighter showLineNumbers className="h-full rounded-b-md" language="javascript" style={vs2015}>
+      {codeString}
+    </SyntaxHighlighter>
+    </div>
+  )
 }
 
-
-export default Example;
+export default Example
