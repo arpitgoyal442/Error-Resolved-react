fbeef732d6f4e775a46ab760dae39c08097bb59b
arpitgoyal442 <arpitgoyal442@gmail.com>
2022-09-07 19:40:51 +0530

    redirects file--for netlify

--- src/Globals/Constants.js
+++ src/Globals/Constants.js
@@ -1,8 +1,8 @@
 const URL="https://errorresolved.herokuapp.com";
 // const URL="http://localhost:9000"
-const front_URL="http://localhost:3000"
+// const front_URL="http://localhost:3000"
 
-// const front_URL="https://errorresolved.netlify.app";
+const front_URL="https://errorresolved.netlify.app";
 
 
 // https://git.heroku.com/errorresolved.git
